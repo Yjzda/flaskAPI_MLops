0e6f3a982b0de5534a0e7cadd0f80d4fc77a683d
Yijia <zeng.yi.jia@hotmail.com>
2023-10-03 14:22:43 +0200

    get coder en dur les predictions

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/predict")
+async def prediction():
+        # Liste de prédictions codée en dur
+    predictions=["Prediction 1", "Prediction 2", "Prediction 3"]
+    
+    return {"predictions":predictions}
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
