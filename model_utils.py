99e5288aec5f85a1673b12e69db934c4622d2815
Yijia <zeng.yi.jia@hotmail.com>
2023-10-03 15:53:05 +0200

    adapt model utils py with build model content

--- model_utils.py
+++ model_utils.py
@@ -1,14 +1,13 @@
 import joblib
-
+import numpy as np
+model_path="regression.joblib"
 def load_model(model_path):
-    try:
-        loaded_model = joblib.load(model_path)
-        return loaded_model
-    except Exception as e:
-        return f"Error loading the model: {str(e)}"
-def predict(loaded_model, input_data):
-    try:
-        predictions = loaded_model.predict(input_data)
-        return predictions
-    except Exception as e:
-        return f"Error making predictions: {str(e)}"
+   
+    loaded_model=joblib.load(model_path)
+    return loaded_model
+    
+
+def predict(input_data,loaded_model):
+  
+    predictions = loaded_model.predict(input_data)
+    return predictions
