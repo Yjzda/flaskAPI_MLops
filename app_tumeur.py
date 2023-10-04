f9c2253ed1e04661f23c032f11d4bbb9608622b0
Yijia <zeng.yi.jia@hotmail.com>
2023-10-04 17:08:14 +0200

    corriger pour avoir dans insominia aussi la possibilite denvoyer plusieurs features, insomnia json : list de dico, il faut refaire le boucle for dans app py

--- app_tumeur.py
+++ app_tumeur.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from model_utils_random import predict_cancer 
+from typing import List
 app = FastAPI()
 
 class TumorFeatures(BaseModel):
@@ -8,6 +9,12 @@ class TumorFeatures(BaseModel):
     p53_concentration: float
 
 @app.post("/predict_cancer")
-async def predict_tumor(tumor_features: TumorFeatures):
-    result = predict_cancer(tumor_features)  
-    return {"prediction": result}
+async def predict_tumor(tumor_features_list: List[TumorFeatures]):
+    results=[]
+    # for tumor_features in tumor_features_list:
+    #     results.append(predict_cancer(tumor_features))
+
+    results=[predict_cancer(tumor_features) for tumor_features in tumor_features_list]
+
+       # result = predict_cancer(tumor_features)  
+    return {"predictions": results}
